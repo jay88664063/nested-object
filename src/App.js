d98428e8a37409329210e20503dfe0abb57e26e3
Jay <jaynandarbarwala1122@gmail.com>
2023-12-14 19:41:18 +0530

    firts comit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Demo from './component/Demo';
+
+
+function App() {
+  return (
+    <Demo/>
+  );
+}
+
+export default App;
---
+++ src/component/Demo.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+function Show() {
+    return <h1>HELLOW JENNY</h1>
+}
+
+function Info() {
+    return (
+        <div>
+            <h1>JAY</h1>
+            <Show />
+        </div>
+    )
+}
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>NANDARBARWALA</h1>
+                < Info />
+            </div>
+        )
+    }
+}
+
+export default Demo
